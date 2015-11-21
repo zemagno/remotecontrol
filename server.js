10f91cca6a5c43827d14384d5fe0e2f823f85572
Jose Carlos Magno <zemagno@gmail.com>
2015-11-21 13:56:49 -0200

    teste

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var express = require('express');
 var io = require('socket.io')(server);
 var cors = require('cors')
 
+//console.log("Iniciando. Listen port 3000...");
+
 app.use(cors());
 
 var routes = require('./routes');
@@ -12,19 +14,19 @@ server.listen(3000);
 
 app.use('/',routes);
 
-var handleClient = function (socket) {
-    // we've got a client connection
-    // socket.emit("tweet", {user: "nodesource", text: "Hello, world!"});
-    console.log("conexao...");
-};
+// var handleClient = function (socket) {
+//     // we've got a client connection
+//     // socket.emit("tweet", {user: "nodesource", text: "Hello, world!"});
+//     console.log("conexao...");
+// };
 
-var receiveByteIo = function (data) {
-    // we've got a client connection
-    console.log("recebi cmd..")
-    console.log(data)
-};
+// var receiveByteIo = function (data) {
+//     // we've got a client connection
+//     console.log("recebi cmd..")
+//     console.log(data)
+// };
 
-io.on("connection", handleClient);
-io.on("message",receiveByteIo);
+// io.on("connection", handleClient);
+// io.on("message",receiveByteIo);
 
 
